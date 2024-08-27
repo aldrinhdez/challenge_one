ac5cc30fe6473097372e3b729401efc7a12a1974
aldrinhdez <aldrinhdzlp@gmail.com>
2024-08-26 22:22:50 -0600

    Subiendo proyecto completo

---
+++ script.js
@@ -0,0 +1,65 @@
+const textArea = document.querySelector(".text-area");
+const mensaje = document.querySelector(".mensaje");
+
+//La letra "e" es convertida para "enter"
+//La letra "i" es convertida para "imes"
+//La letra "a" es convertida para "ai"
+//La letra "o" es convertida para "ober"
+//La letra "u" es convertida para "ufat"
+function btnEncriptar(){
+    const textoEncriptado = encriptar(textArea.value);
+    mensaje.value = textoEncriptado;
+    textArea.value = "";
+    mensaje.style.backgroundImage = "none";
+}
+
+function encriptar(stringEncriptado){
+    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+    stringEncriptado = stringEncriptado.toLowerCase();
+
+    for(let i = 0; i < matrizCodigo.length; i ++){
+        if(stringEncriptado.includes(matrizCodigo[i][0])){
+            stringEncriptado = stringEncriptado.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1]);
+
+        }
+    }
+    return stringEncriptado
+}
+
+
+function btnDesencriptar(){
+    const textoEncriptado = desencriptar(textArea.value);
+    mensaje.value = textoEncriptado;
+    textArea.value = "";
+
+}
+
+function desencriptar(stringDesencriptado){
+    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+    stringDesencriptado = stringDesencriptado.toLowerCase();
+
+    for(let i = 0; i < matrizCodigo.length; i ++){
+        if(stringDesencriptado.includes(matrizCodigo[i][1])){
+            stringDesencriptado = stringDesencriptado.replaceAll(matrizCodigo[i][1], matrizCodigo[i][0]);
+
+        }
+    }
+    return stringDesencriptado
+}
+
+ 
+
+document.addEventListener('DOMContentLoaded', function() {
+    const mensajeTextarea = document.querySelector('.mensaje');
+    const copiarButton = document.querySelector('.copiar');
+
+    copiarButton.addEventListener('click', async function() {
+        try {
+            await navigator.clipboard.writeText(mensajeTextarea.value);
+            alert('Texto copiado al portapapeles');
+        } catch (err) {
+            console.error('Error al copiar texto: ', err);
+            alert('No se pudo copiar el texto');
+        }
+    });
+});
